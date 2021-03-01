3866a14bef21bd2ff10601473a03e9977dacfa21
NicolasGffn <78535763+NicolasGffn@users.noreply.github.com>
2021-03-01 10:36:01 +0100

    Update Creation_CNN.py

--- Creation_CNN.py
+++ Creation_CNN.py
@@ -45,8 +45,7 @@ for layer in model.layers:
 output_vgg16_conv = model.output
 
 
-# Ajouter la nouvelle couche fully-connected pour la classification à 3 classes
-
+# Ajouter les npouvelles couches fully-connected pour la classification à 3 classes
 x = Flatten(name='flatten')(output_vgg16_conv)
 x = Dense(4096, activation='relu', name='fc1')(x)
 x = Dropout(0.3)(x)
@@ -58,11 +57,6 @@ x = Dense(3, activation='softmax')(x)
 # Définir le nouveau modèle
 CNN = keras.models.Model(inputs=model.input, outputs=x)
 
-
-
-
-
-
 # Compiler le modèle 
 CNN.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -71,130 +65,45 @@ CNN.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accurac
 
 
 
-
-
-DIRECTORY = "C:\\Users\\nicol\\.spyder-py3\\Face Mask Dataset\\Train"
-
-CATEGORIES = ['WithoutMask', 'WithMask', 'IncorrectlyWornMask']
-IMG_SIZE = 64 # IMG_SIZE = 224 alternative size
-
-# #data
-# X = []
-# #labels(0,1,2)
-# y = []
-
-# def create_data():
-#     for category in CATEGORIES:
-#         path = os.path.join(DIRECTORY, category)
-#         class_num_label = CATEGORIES.index(category)
-#         for img in os.listdir(path):
-#             try:
-#                 img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_COLOR)
-#                 img_array = cv2.resize(img_array, (IMG_SIZE,IMG_SIZE))
-#                 X.append(img_array)
-#                 y.append(class_num_label)
-#             except Exception as e:
-#                 pass
-            
-# create_data()
-
-
-# # Get images as a 4,096 feature set
-# SAMPLE_SIZE = len(y)
-# data = np.array(X).flatten().reshape(SAMPLE_SIZE, IMG_SIZE*IMG_SIZE, 3) # pixel-features
-
-# # Turn X and y into numpy arrays
-# X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3) # images
-# y = np.array(y) # target
-
-# # print("Features, X shape: ", X.shape)
-# # print("Target, y shape: ", y.shape)
-# # print("Data shape: ", data.shape)
-
-
-# # Saves us from having to regenerate our data by saving our data
-# pickle_out = open("X.pickle", "wb")
-# pickle.dump(X, pickle_out)
-# pickle_out.close()
-
-# pickle_out = open("y.pickle", "wb")
-# pickle.dump(y, pickle_out)
-# pickle_out.close()
-
-# pickle_out = open("data.pickle", "wb")
-# pickle.dump(data, pickle_out)
-# pickle_out.close()
-
-
-
+# On charge les données générées
 pickle_in = open("X.pickle", "rb")
 X = pickle.load(pickle_in)
 pickle_in = open("y.pickle", "rb")
 y = pickle.load(pickle_in)
-# pickle_in = open("data.pickle", "rb")
-# data = pickle.load(pickle_in)
 
+
+# Affichage des caractéristiques des jeux de données
 print('# of Samples:', len(y))
 print('# of Without A Mask:', (y == 0).sum())
 print('# of With A Mask:', (y == 1).sum())
 print('# of With An Incorrectly Worn Mask:', (y == 2).sum())
 
 
-print('Shape of X:', X.shape)
-print('Shape of y:', y.shape)
-
-
-
-
-# # resized_X = []
-# # for img in X:
-# #     resized_X.append(cv2.resize(img, (64, 64)))
-
-# # X = np.asarray(resized_X)
-# X = X.reshape(-1, 64, 64, 3)
-# print(X.shape)
-
-
-# normalize the pixel values
+# Affichage test d'une image
+plt.imshow(X[5014])
+plt.show()
+   
+# On normalise les valeurs des pixels
 X = X / 255
 
-# plt.imshow(X[5014])
-# plt.show()
-   
-# IMG_DIM = X.shape[1]
-# print('IMG_DIM:',IMG_DIM)
-
-
-
-
-# # Split our data into testing and training
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=45)
-
-# y_train = tf.keras.utils.to_categorical(y_train, num_classes=None, dtype='float32')
-# y_test = tf.keras.utils.to_categorical(y_test, num_classes=None, dtype='float32')
-
+# Transformation de y nécessaire pour l'entraînement
 y = tf.keras.utils.to_categorical(y, num_classes=None, dtype='float32')
 
-
-# Print the length and width of our testing data.
-# print('Length of our Training data: ', len(X_train), '\nLength of our Testing data: ', len(X_test))
-
-
-# Entraîner sur les données d'entraînement (X_train, y_train)
+# Entraînement sur les données X et y
 history = CNN.fit(X, y, epochs=20, verbose=1)
 
-# Summarize history for train
+
+# Affichage des courbes de précision et de perte
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['loss'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
+plt.title('Précision et perte du modèle')
+plt.ylabel('Précision/Perte')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='right')
+plt.legend(['Précision', 'Perte'], loc='right')
 plt.show()
 
 
 
 
-
-CNN.save('C:\\Users\\nicol\\.spyder-py3\\CNN_perso.h5')
-
+# Sauvegarde du modèle entraîné
+CNN.save(Lien du fichier)
